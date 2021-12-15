7d2230de18cd6d322b654295313b808288e9abd8
paigeman <53284808+paigeman@users.noreply.github.com>
2021-12-15 18:00:15 +0800

    set method isNotNull and isNull to default

--- FieldValidateAnnotation/fva-version-2/src/main/java/org/fade/demo/annotationdemo/fieldvalidateannotation/version2/validate/Validator.java
+++ FieldValidateAnnotation/fva-version-2/src/main/java/org/fade/demo/annotationdemo/fieldvalidateannotation/version2/validate/Validator.java
@@ -8,4 +8,38 @@ package org.fade.demo.annotationdemo.fieldvalidateannotation.version2.validate;
  */
 public interface Validator {
 
+    /**
+     * 校验是否不为null
+     * */
+    default void isNotNull() {
+        
+    }
+
+    /**
+     * 校验是否为null
+     * */
+    default void isNull() {
+
+    }
+
+    /**
+     * 校验是否不为blank
+     * */
+    void isNotBlank();
+
+    /**
+     * 校验是否为blank
+     * */
+    void isBlank();
+
+    /**
+     * 校验是否不为empty
+     * */
+    void isNotEmpty();
+
+    /**
+     * 校验是否为empty
+     * */
+    void isEmpty();
+
 }
